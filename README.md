455a052596071bf3e6b43c96c39afb385cb63031
Anton Bugryakov <antis_amdg@mail.ru>
2022-01-09 18:38:41 +0300

    commit-1-hw-done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/a1564w0kvxdphmr1/branch/main?svg=true)](https://ci.appveyor.com/project/Antis85/ra-hw9-router-menu/branch/main)
+
 # ra-hw9-router-menu
+
+[Open app](https://antis85.github.io/ra-hw9-router-menu)
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Menu from './components/Menu';
+import DriftPage from './components/DriftPage';
+import TimeAttackPage from './components/TimeAttackPage';
+import ForzaPage from './components/ForzaPage';
+import HomePage from './components/HomePage';
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Menu />
+        <div className="page">
+          <Routes>
+            <Route path="/drift" element={<DriftPage />} />
+            <Route path="/timeattack" element={<TimeAttackPage />} />
+            <Route path="/forza" element={<ForzaPage />} />
+            <Route exact path="/" element={<HomePage />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
